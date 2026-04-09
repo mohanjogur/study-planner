bf48bdecc3452657acb840f1d7ab36150463c9ad
mohanjogur <mohanjogur21@gmail.com>
2026-04-09 14:59:22 +0530

    UI upgraded to modern app design

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ function addTask() {
     if (task === "") return;
 
     let li = document.createElement("li");
-    li.innerHTML = task + " <button onclick='removeTask(this)'>❌</button>";
+
+    let span = document.createElement("span");
+    span.innerText = task;
+
+    // Mark as complete
+    span.onclick = function () {
+        span.classList.toggle("completed");
+    };
+
+    let btn = document.createElement("button");
+    btn.innerText = "❌";
+    btn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(span);
+    li.appendChild(btn);
 
     document.getElementById("taskList").appendChild(li);
     input.value = "";
-}
-
-function removeTask(btn) {
-    btn.parentElement.remove();
 }
